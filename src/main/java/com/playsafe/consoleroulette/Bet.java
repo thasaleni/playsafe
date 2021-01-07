aa4c09a8089b23c4bb0a58b45de286d6e476a97e
Phumzile Saleni <thasaleni@gmail.com>
2021-01-07 10:45:30 +0200

    last commit

--- src/main/java/com/playsafe/consoleroulette/Bet.java
+++ src/main/java/com/playsafe/consoleroulette/Bet.java
@@ -52,6 +52,34 @@ public class Bet {
   public void setWinnings(Double winnings) {
     this.winnings = winnings;
   }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((amount == null) ? 0 : amount.hashCode());
+    result = prime * result + ((bet == null) ? 0 : bet.hashCode());
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    Bet other = (Bet) obj;
+    if (amount == null) {
+      if (other.amount != null) return false;
+    } else if (!amount.equals(other.amount)) return false;
+    if (bet == null) {
+      if (other.bet != null) return false;
+    } else if (!bet.equals(other.bet)) return false;
+    if (name == null) {
+      if (other.name != null) return false;
+    } else if (!name.equals(other.name)) return false;
+    return true;
+  }
 }
 
 enum Outcome {
--- src/main/java/com/playsafe/consoleroulette/ConsoleRoulette.java
+++ src/main/java/com/playsafe/consoleroulette/ConsoleRoulette.java
@@ -1,18 +1,15 @@
 package com.playsafe.consoleroulette;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Random;
-import java.util.stream.Stream;
+import java.util.Set;
 
+import org.apache.commons.lang3.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,67 +20,63 @@ public class ConsoleRoulette {
 
   public static void main(String[] args) {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    System.out.println("please enter player file path.\n");
     try {
-      String path = reader.readLine();
-      File pathFile = new File(path);
-      if (path != null && pathFile.exists() && pathFile.canRead()) {
-        Stream<String> stream = Files.lines(Paths.get(path));
-        System.out.println("please enter bet in format name bet amount, eg. Tiki_Monkey 2 1.0 \n");
-        String[] betSplit = reader.readLine().split(" ");
-        Bet bet =
-            new Bet(betSplit[0], Double.parseDouble(betSplit[1]), Double.parseDouble(betSplit[2]));
-        Instant start = Instant.now();
-        List<Bet> bets = new ArrayList<Bet>();
-        while (bet != null && bet.getName() != null) {
-          bets.add(bet);
-          if (Duration.between(start, Instant.now()).getSeconds() >= 30) {
-            start = Instant.now();
-            int result = new Random().nextInt(36 - 0 + 1) + 0;
-            for (Bet playedBet : bets) {
-              if ((playedBet.getBet().getClass().isInstance(Integer.class)
-                  && result == (Integer) playedBet.getBet())) {
+      System.out.println(
+          "please enter bet in format name bet amount, eg. Tiki_Monkey 2 1.0  and press Enter\n");
+      String[] betSplit = reader.readLine().split(" ");
+      Bet bet =
+          new Bet(betSplit[0], betSplit[1], Double.parseDouble(betSplit[2]));
+      Instant start = Instant.now();
+      Set<Bet> bets = new HashSet<Bet>();
+      bets.add(bet);
+      while (bet != null && bet.getName() != null) {
+        if (Duration.between(start, Instant.now()).getSeconds() >= 30) {
+          start = Instant.now();
+          int result = new Random().nextInt(36 - 0 + 1) + 0;
+          for (Bet playedBet : bets) {
+            if ((NumberUtils.isParsable(playedBet.getBet().toString())
+                && result == Integer.parseInt(playedBet.getBet().toString()))) {
+              playedBet.setOutcome(Outcome.WIN);
+              playedBet.setWinnings(36 *  playedBet.getAmount());
+            } else if (playedBet.getBet().getClass().equals(String.class)) {
+              if (playedBet.getBet().toString().equalsIgnoreCase("EVEN") && result % 2 == 0) {
                 playedBet.setOutcome(Outcome.WIN);
-                playedBet.setAmount(36.0 * (Integer) playedBet.getBet());
-              } else if (playedBet.getBet().getClass().isInstance(String.class)) {
-                if (playedBet.getBet().toString().equalsIgnoreCase("EVEN") && result % 2 == 0) {
-                  playedBet.setOutcome(Outcome.WIN);
-                  playedBet.setAmount((Integer) playedBet.getBet() * 2.0);
-                } else if (playedBet.getBet().toString().equalsIgnoreCase("ODD")
-                    && result % 2 != 0) {
-                  playedBet.setAmount((Integer) playedBet.getBet() * 2.0);
-                  playedBet.setOutcome(Outcome.WIN);
-                } else {
-                  playedBet.setOutcome(Outcome.LOSE);
-                }
+                playedBet.setWinnings(playedBet.getAmount() * 2);
+              } else if (playedBet.getBet().toString().equalsIgnoreCase("ODD") && result % 2 != 0) {
+                playedBet.setWinnings(playedBet.getAmount() * 2);
+                playedBet.setOutcome(Outcome.WIN);
               } else {
+                playedBet.setWinnings(0.0);
                 playedBet.setOutcome(Outcome.LOSE);
               }
-
-              System.out.println(String.format("NUMBER: %d", result));
-              System.out.println("Player     Bet      Outcome     Winnings");
-              System.out.println(
-                  String.format(
-                      "%s     %d      %s     %d",
-                      playedBet.getName(),
-                      playedBet.getBet(),
-                      playedBet.getOutcome(),
-                      playedBet.getWinnings()));
+            } else {
+              playedBet.setWinnings(0.0);
+              playedBet.setOutcome(Outcome.LOSE);
             }
-
-            bets = new ArrayList<Bet>();
+            bets.add(playedBet);
           }
-          reader.readLine().split(" ");
-          bet =
-              new Bet(
-                  betSplit[0], Double.parseDouble(betSplit[1]), Double.parseDouble(betSplit[2]));
+          System.out.println(String.format("NUMBER: %d", result));
+          System.out.println(String.format("Player\tBet\tOutcome\tWinnings"));
+          for (Bet playedBet : bets) {
+            System.out.println(
+                String.format(
+                    "%s\t%s\t%s\t%2f",
+                    playedBet.getName(),
+                    playedBet.getBet(),
+                    playedBet.getOutcome().toString(),
+                    playedBet.getWinnings()));
+          }
+
+          bets = new HashSet<Bet>();
         }
-      } else {
-        System.err.print(
-            String.format(
-                "Path: %s, is malformed, does not exist or unreadable, please check and reload.",
-                pathFile.getAbsoluteFile().toString()));
+        System.out.println(
+            "please enter bet in format name bet amount, eg. Tiki_Monkey 2 1.0 and press Enter\n");
+        betSplit = reader.readLine().split(" ");
+        bet =
+            new Bet(betSplit[0], betSplit[1], Double.parseDouble(betSplit[2]));
+        bets.add(bet);
       }
+
     } catch (IOException e) {
       LOGGER.debug(e.getMessage());
     }
